5b008f6cff180808c1b014b715755f886a68bfa7
huangzilong <huangzilongg@gmail.com>
2018-09-12 22:40:00 +0800

    update help & error

--- cmds/help.js
+++ cmds/help.js
@@ -1,3 +1,21 @@
-module.exports = () => {
-    console.log('help --还没写')
-}
+const menus = {
+    main: `yd <command> <options>
+
+    help .............. 查看帮助
+    type .............. 查看支持的语言类型
+    --version, -v ............ 查看版本
+    search .............. 查询单词
+        --from, -f ............ 设置翻译前语言类型
+        --to, -t ............ 设置需要翻译成哪种语言
+        --word, -w ............ 设置要翻译的文本`,
+    type: `yd type ............. 查看支持的语言`,
+    search: `yd search <options>
+    --version, -v ............ 查看版本
+    --from, -f ............ 设置翻译前语言类型
+    --to, -t ............ 设置需要翻译成哪种语言
+    --word, -w ............ 设置要翻译的文本`,
+};
+module.exports = args => {
+    const subCmd = args._[0] === 'help' ? args._[1] : args._[0];
+    console.log(menus[subCmd] || menus.main);
+};
--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const minimist = require('minimist')
+const error = require('./utils/error')
 
 module.exports = () => {
     const args = minimist(process.argv.slice(2))
     let cmd = args._[0] || 'help'
+    if (args.w || args.word) cmd = 'search'
     if (args.v || args.version) cmd = 'version'
     if (args.h || args.help) cmd = 'help'
-    if (args.w || args.word) cmd = 'search'
 
     switch(cmd) {
         case 'search':
@@ -18,10 +19,13 @@ module.exports = () => {
             require('./cmds/version.js')()
             break
         case 'help':
-            require('./cmds/help.js')()
+            require('./cmds/help.js')(args)
+            break
+        case 'type':
+            require('./cmds/type.js')(args)
             break
         default:
-            console.log(`不支持 ${cmd} 命令`)
+            error(`"${cmd}" is not a valid command!`, true)
             break
     }
 }
